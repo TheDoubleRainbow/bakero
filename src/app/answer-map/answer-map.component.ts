6591099eda38f4de843627b56369b564f4eab517
Andrew <16mb.com@gmail.com>
2019-10-19 21:21:46 +0300

    rework map

--- src/app/answer-map/answer-map.component.ts
+++ src/app/answer-map/answer-map.component.ts
@@ -10,9 +10,16 @@ export class AnswerMapComponent implements OnInit {
   @Input()
   mapData = {
     title: 'Answer map',
-    lat: 51.678418,
-    lng: 7.809007
+    lat: 50,
+    lng: 50
   };
+
+  public marker = {
+    lat: 9999,
+    lng: 9999
+  }
+
+  public zoom = 3
 
   constructor() { }
 
